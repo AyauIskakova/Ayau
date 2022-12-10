fc08f1035cac6c2c90dc449130c511d97b6f0689
AyauIskakova <120235865+AyauIskakova@users.noreply.github.com>
2022-12-10 15:53:59 +0600

    Create Questions.java
    Ayaulym

---
+++ Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: _______");
+        System.out.println("Surname: _______");
+        System.out.println("Id: _________");
+        System.out.println("Hobby: __________");
+        System.out.println("How much do you like ICT(from 1-10)): _______");
+        System.out.println("Rate this task from 1 to 10: _______");
+        System.out.println("My favorite color?: ______");
+    }
+}
