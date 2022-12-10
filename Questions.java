bc9d1b77c013278af3ec4c0d25791a50944faa51
AyauIskakova <120235865+AyauIskakova@users.noreply.github.com>
2022-12-10 16:02:33 +0600

    changeed

--- Questions.java
+++ Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _________");
-        System.out.println("Hobby: __________");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("My favorite color?: ______");
+        System.out.println("Name: Ayaulym");
+        System.out.println("Surname: jkl");
+        System.out.println("Id: 6454");
+        System.out.println("Hobby: ghx");
+        System.out.println("How much do you like ICT(from 1-10)): 5");
+        System.out.println("Rate this task from 1 to 10: 8");
+        System.out.println("My favorite color?: jh");
     }
 }
